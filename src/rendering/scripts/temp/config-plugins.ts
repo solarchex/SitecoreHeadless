be682d242a47030e6c5d633d557b8b76efbf3959
Rashmy Patwari <rashmy.patwari@gmail.com>
2023-06-13 10:04:27 -0400

    Created
    XM + SXA + Headless

---
+++ src/rendering/scripts/temp/config-plugins.ts
@@ -0,0 +1,5 @@
+export { computedPlugin } from 'scripts/config/plugins/computed';
+export { disconnectedPlugin } from 'scripts/config/plugins/disconnected';
+export { fallbackPlugin } from 'scripts/config/plugins/fallback';
+export { packageJsonPlugin } from 'scripts/config/plugins/package-json';
+export { scjssconfigPlugin } from 'scripts/config/plugins/scjssconfig';
